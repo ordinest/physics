f052df2d357beed2365ab9558a0ec987c82208ba
Taylor M Ordines <ordinest@gmail.com>
2020-09-28 10:52:34 -0400

    Updates

---
+++ precache-manifest.6841c444d52a7262f2b8b6276b0ba30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e377a2c3630cc2a899ba53547ec30d9b",
+    "url": "/physics/index.html"
+  },
+  {
+    "revision": "42395b4927d15b2f0703",
+    "url": "/physics/static/css/main.3efa7240.chunk.css"
+  },
+  {
+    "revision": "41ce6a8de894adf12379",
+    "url": "/physics/static/js/2.307df87c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/physics/static/js/2.307df87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42395b4927d15b2f0703",
+    "url": "/physics/static/js/main.71d307df.chunk.js"
+  },
+  {
+    "revision": "9c5c119a509af7c246d2",
+    "url": "/physics/static/js/runtime-main.04de2196.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/physics/precache-manifest.6841c444d52a7262f2b8b6276b0ba30d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/physics/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
