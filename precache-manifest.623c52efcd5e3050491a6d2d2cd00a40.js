29f17df0a821f9071e88a20fa952c50abf01bc07
Taylor M Ordines <ordinest@gmail.com>
2020-09-29 12:27:27 -0400

    Updates

---
+++ precache-manifest.623c52efcd5e3050491a6d2d2cd00a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91970909c2722b0349b7f594121379d",
+    "url": "/physics/index.html"
+  },
+  {
+    "revision": "3f7cba5ae07f1c9d6e01",
+    "url": "/physics/static/css/main.3efa7240.chunk.css"
+  },
+  {
+    "revision": "e6bd8c1460189056aa73",
+    "url": "/physics/static/js/2.8ccbbe62.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/physics/static/js/2.8ccbbe62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7cba5ae07f1c9d6e01",
+    "url": "/physics/static/js/main.3c4503b1.chunk.js"
+  },
+  {
+    "revision": "9c5c119a509af7c246d2",
+    "url": "/physics/static/js/runtime-main.04de2196.js"
+  }
+]);
